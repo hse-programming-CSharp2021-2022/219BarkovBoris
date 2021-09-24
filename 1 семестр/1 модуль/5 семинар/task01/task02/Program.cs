90451e79de6ec824846f8dd30312c5156008abd5
HSEBarkovBG <bgbarkov@edu.hse.ru>
2021-09-24 13:40:54 +0300

    upd

---
+++ 1 семестр/1 модуль/5 семинар/task01/task02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task02
+{
+    class Program
+    {
+        // 1 2 4 8
+        static int binPow(int x, int p)
+        {
+            int ans = 0;
+            for (int i = 0; i < p; i++)
+            {
+                ans += ans;
+                ans += 1;
+            }
+            return ans + 1;
+        }
+
+        static void Main()
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+            int.TryParse(Console.ReadLine(), out int m);
+            int x = binPow(2, n) + binPow(2, m);
+            Console.WriteLine(x);
+        }
+    }
+}
