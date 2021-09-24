4af12482e445d4ffc07e07be7af7f93ea34be07c
HSEBarkovBG <bgbarkov@edu.hse.ru>
2021-09-24 14:21:43 +0300

    upd

---
+++ 1 семестр/1 модуль/5 семинар/task01/task03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task03
+{
+    class Program
+    {
+        static void swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (i + 1 < n)
+                {
+                    if ((arr[i] + arr[i + 1]) % 3 == 0)
+                    {
+                        arr[i] = arr[i] * arr[i + 1];
+                        for (int j = i + 1; j + 1 < n; j++)
+                        {
+                            swap(ref arr[j], ref arr[j + 1]);
+                        }
+                        n--;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] != -1)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
